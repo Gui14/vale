566c56dfbddd2f72bdcfaad7ce6cf8e0dcd3cf64
Gui14 <guilhermepimentelgui@gmail.com>
2023-11-16 10:39:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('botao').addEventListener('click', function() {
+    var inputValue = document.getElementById('input').value;
+  
+    if (inputValue === "") {
+      alert("Por favor, digite a data");
+    } else {
+      // Redirecionar ou executar a ação desejada
+      window.location.href = `https://api.whatsapp.com/send?phone=73982004078&text=Quero meu dia de princesa este dia: ${inputValue}`;
+    }
+  });
+  
